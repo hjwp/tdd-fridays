61ce8c2c5ce4081ac88b5dc3bdd49060e16ec49a
James Gough <james.gough@made.com>
2019-02-01 14:16:54 +0000

    another commit

--- test_calculator.py
+++ test_calculator.py
@@ -5,11 +5,9 @@ def test_add_one_to_one():
     result = add('I', 'I')
     assert result == 'II'
 
-
 def test_one_to_two():
     result = add('I', 'II')
     assert result == 'III'
-
 
 def test_two_to_one():
     result = add('II', 'I')
@@ -23,14 +21,16 @@ def test_two_to_three():
     result = add('II', 'III')
     assert result == 'V'
 
-
 def test_one_to_five():
     result = add('I', 'V')
     assert result == 'VI'
     result = add('V', 'I')
     assert result == 'VI'
 
-
 def test_five_to_five():
     result = add('V', 'V')
     assert result == 'X'
+
+def test_add_30_to_20():
+    result = add('XXX', 'XX')
+    assert result == 'L'
