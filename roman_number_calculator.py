160569357165576b982438156c1b91f9f3b4effe
Harry <hjwp2@cantab.net>
2019-02-01 14:31:05 +0000

    another commit

--- roman_number_calculator.py
+++ roman_number_calculator.py
@@ -1,5 +1,8 @@
-def add(first, second):
-    smooshed = ''.join(reversed(sorted(f"{first}{second}")))
-    result = smooshed.replace('IIIII', 'V').replace('VV', 'X').replace("XXXXX", "L")
 
+def roman_number_order(n):
+    return 'IVXL'.index(n)
+
+def add(first, second):
+    smooshed = ''.join(reversed(sorted(f"{first}{second}", key=roman_number_order)))
+    result = smooshed.replace('IIIII', 'V').replace('VV', 'X').replace("XXXXX", "L")
     return result
--- test_calculator.py
+++ test_calculator.py
@@ -1,40 +1,25 @@
 from roman_number_calculator import add
+import pytest
 
 
-def test_add_i_to_i():
-    result = add('I', 'I')
-    assert result == 'II'
+@pytest.mark.parametrize('first,second,expected', [
+    ('I', 'I', 'II'),
+    ('I', 'II', 'III'),
+    ('II', 'I', 'III'),
+    ('II', 'III', 'V'),
+    ('I', 'V', 'VI'),
+    ('V', 'I', 'VI'),
+    ('V', 'V', 'X'),
+    ('XXX', 'XX', 'L'),
+    ('LX', 'VI', 'LXVI'),
 
-def test_i_to_ii():
-    result = add('I', 'II')
-    assert result == 'III'
+])
+def test_old_numerals(first, second, expected):
+    assert add(first, second) == expected
 
-def test_ii_to_i():
-    result = add('II', 'I')
-    assert result == 'III'
 
-def test_ii_to_ii():
-    result = add('II', 'II')
-    assert result == 'IIII'  # TODO: support modern roman numerals one day
-
-def test_ii_to_iii():
-    result = add('II', 'III')
-    assert result == 'V'
-
-def test_i_to_v():
-    result = add('I', 'V')
-    assert result == 'VI'
-    result = add('V', 'I')
-    assert result == 'VI'
-
-def test_v_to_v():
-    result = add('V', 'V')
-    assert result == 'X'
-
-def test_add_xxx_to_xx():
-    result = add('XXX', 'XX')
-    assert result == 'L'
-
-def test_add_lx_to_vi():
-    result = add('LX', 'VI')
-    assert result == 'LXVI'
+@pytest.mark.parametrize('first,second,expected', [
+    ('II', 'II', 'IV'),
+])
+def test_backwardsey_numerals(first, second, expected):
+    assert add(first, second) == expected
