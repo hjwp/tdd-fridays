4f0140c977074b939f99a7f2190d006061f5b9f8
Pavel Krayzel <pavel.krayzel@made.com>
2019-02-01 14:22:51 +0000

    another commit

--- roman_number_calculator.py
+++ roman_number_calculator.py
@@ -1,5 +1,5 @@
 def add(first, second):
     smooshed = ''.join(reversed(sorted(f"{first}{second}")))
-    result = smooshed.replace('IIIII', 'V').replace('VV', 'X')
+    result = smooshed.replace('IIIII', 'V').replace('VV', 'X').replace("XXXXX", "L")
 
     return result
--- test_calculator.py
+++ test_calculator.py
@@ -1,36 +1,40 @@
 from roman_number_calculator import add
 
 
-def test_add_one_to_one():
+def test_add_i_to_i():
     result = add('I', 'I')
     assert result == 'II'
 
-def test_one_to_two():
+def test_i_to_ii():
     result = add('I', 'II')
     assert result == 'III'
 
-def test_two_to_one():
+def test_ii_to_i():
     result = add('II', 'I')
     assert result == 'III'
 
-def test_two_to_two():
+def test_ii_to_ii():
     result = add('II', 'II')
     assert result == 'IIII'  # TODO: support modern roman numerals one day
 
-def test_two_to_three():
+def test_ii_to_iii():
     result = add('II', 'III')
     assert result == 'V'
 
-def test_one_to_five():
+def test_i_to_v():
     result = add('I', 'V')
     assert result == 'VI'
     result = add('V', 'I')
     assert result == 'VI'
 
-def test_five_to_five():
+def test_v_to_v():
     result = add('V', 'V')
     assert result == 'X'
 
-def test_add_30_to_20():
+def test_add_xxx_to_xx():
     result = add('XXX', 'XX')
     assert result == 'L'
+
+def test_add_lx_to_vi():
+    result = add('LX', 'VI')
+    assert result == 'LXVI'
