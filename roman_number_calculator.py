8fe10dd7b0033be5ab7bbc79e7bf413329f5f2ff
James Gough <james.gough@made.com>
2019-02-01 14:03:37 +0000

    another commit

--- roman_number_calculator.py
+++ roman_number_calculator.py
@@ -1,4 +1,2 @@
 def add(first, second):
-    if second == 'II':
-        return 'III'
-    return "II"
+    return f"{first}{second}"
--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,13 @@ def test_add_one_to_one_no_class():
 def test_one_to_two_no_class():
     result = add('I', 'II')
     assert result == 'III'
+
+
+def test_two_to_one_no_class():
+    result = add('II', 'I')
+    assert result == 'III'
+
+
+def test_two_to_three_no_class():
+    result = add('II', 'III')
+    assert result == 'V'
