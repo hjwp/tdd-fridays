81bd687ef083fa405ecd14467e13ff9c69dc340c
James Gough <james.gough@made.com>
2019-02-01 14:37:19 +0000

    another commit

--- roman_number_calculator.py
+++ roman_number_calculator.py
@@ -1,11 +1,9 @@
-
 def roman_number_order(n):
     return 'IVXL'.index(n)
 
 def add(first, second):
     smooshed = first + second
-
     ordered = ''.join(reversed(sorted(smooshed, key=roman_number_order)))
     old_style_result = ordered.replace('IIIII', 'V').replace('VV', 'X').replace("XXXXX", "L")
-    new_style_result = old_style_result.replace('IV', 'IIII')
+    new_style_result = old_style_result.replace('IIII', 'IV')
     return new_style_result
