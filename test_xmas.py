8172b138d7d3480aa5c51d85682c4ccc11b91fb3
Illia Sukonnik <illia.sukonnik@made.com>
2019-02-15 13:35:43 +0000

    added turn_off

--- test_xmas.py
+++ test_xmas.py
@@ -1,5 +1,6 @@
 from xmas import Grid
 
+
 def test_default_count_is_zero():
     grid = Grid()
     assert grid.lit_count == 0
@@ -10,6 +11,12 @@ def test_turn_on_first_light():
     grid.act(command)
     assert grid.lit_count == 1
 
+def test_off_on_first_light():
+    grid = Grid()
+    command = "turn off 0,0 through 0,0"
+    grid.act(command)
+    assert grid.lit_count == 0
+
 def test_toggle_first_light():
     grid = Grid()
     command = "turn on 0,0 through 0,0"
@@ -18,7 +25,3 @@ def test_toggle_first_light():
     command = "toggle 0,0 through 0,0"
     grid.act(command)
     assert grid.lit_count == 0
-
-
-
-
