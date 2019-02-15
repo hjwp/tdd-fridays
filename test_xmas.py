37f1af1edd24d11c97f09839555183d1a14c293d
Jon Bramley <jon.bramley@made.com>
2019-02-15 14:22:46 +0000

    another commit

--- test_xmas.py
+++ test_xmas.py
@@ -32,28 +32,7 @@ def test_toggle_first_light():
     grid.act(command)
     assert grid.lit_count == 1
 
-def test_turn_on_two_lights():
+def test_turn_on_two_lights_in_a_range():
     grid = Grid()
-    grid.act("turn on 0,0 through 0,0")
-    grid.act("turn on 0,1 through 0,1")
+    grid.act("turn on 0,0 through 0,1")
     assert grid.lit_count == 2
-
-def test_turn_two_lights_on_then_off():
-    grid = Grid()
-    grid.act("turn on 0,0 through 0,0")
-    grid.act("turn on 0,1 through 0,1")
-    assert grid.lit_count == 2
-    grid.act("turn off 0,0 through 0,0")
-    assert grid.lit_count == 1
-    grid.act("turn off 0,1 through 0,1")
-    assert grid.lit_count == 0
-
-
-def test_turn_off_same_light_twice():
-    grid = Grid()
-    grid.act("turn on 0,0 through 0,0")
-    grid.act("turn on 0,1 through 0,1")
-    grid.act("turn off 0,0 through 0,0")
-    assert grid.lit_count == 1
-    grid.act("turn off 0,0 through 0,0")
-    assert grid.lit_count == 1
--- xmas.py
+++ xmas.py
@@ -8,19 +8,18 @@ class Grid:
         return len(self.lit)
 
     def act(self, command):
-        pos = re.search(r'.+(\d,\d).+', command).group(1)
+        pos1, pos2 = re.search(r'.+(\d,\d) through (\d,\d)', command).groups()
 
-        if 'toggle' in command:
-            if pos in self.lit:
-                self.lit.remove(pos)
-            else:
+        for pos in {pos1, pos2}:
+            if 'toggle' in command:
+                if pos in self.lit:
+                    self.lit.remove(pos)
+                else:
+                    self.lit.add(pos)
+
+            if 'turn on' in command:
                 self.lit.add(pos)
 
-        if 'turn on' in command:
-            self.lit.add(pos)
-
-        if 'turn off' in command:
-            try:
-                self.lit.remove(pos)
-            except KeyError:
-                pass
+            if 'turn off' in command:
+                if pos in self.lit:
+                    self.lit.remove(pos)
