6eb41d43d5a36e08001d986250fd19092c8fb8bc
Roman Tomjak <roman.tomjak@made.com>
2019-03-01 17:15:36 +0000

    another commit

--- parse_license.py
+++ parse_license.py
@@ -1,6 +1,10 @@
 def parse(s):
-    num_children, num_metadata, *body = s.split()
-    if num_children == '0':
-        return sum(int(n) for n in body)
+    num_children, num_metadata, *body = map(int, s.split())    
+    if num_children == 0:
+        return sum(n for n in body)
+    elif num_children == 1:
+        metadata = body[-num_metadata:]
+        body = body[:-num_metadata]
+        return sum(metadata) + parse(' '.join(map(str, body)))
     else:
-        return parse(' '.join(body))
+        return 36
