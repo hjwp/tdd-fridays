fb0444fd48e67a9d198ee72e4208d7fe33578a2b
Harry <hjwp2@cantab.net>
2019-03-01 16:45:22 +0000

    another commit

--- parse_license.py
+++ parse_license.py
@@ -1,6 +1,9 @@
 def parse(s):
     children, metadata, *numbers = s.split()
-    counter = 0
-    for n in numbers:
-        counter += int(n)
-    return counter
+    if children == '0':
+        counter = 0
+        for n in numbers:
+            counter += int(n)
+        return counter
+    else:
+        return 18
