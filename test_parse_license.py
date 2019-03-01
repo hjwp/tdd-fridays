b9791b4df93a37191fa6cca36c128c146faa2c5c
Roman Tomjak <roman.tomjak@made.com>
2019-03-01 16:42:45 +0000

    another commit

--- test_parse_license.py
+++ test_parse_license.py
@@ -3,3 +3,7 @@ from parse_license import parse
 def test_no_children_sums_metadata():
     assert parse('0 3 4 5 6') == 4 + 5 + 6
     assert parse('0 2 0 7') == 7
+
+
+def test_one_child():
+    assert parse('1 2 0 1 9 2 7') == 18
