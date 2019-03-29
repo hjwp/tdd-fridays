57cf411dc977f5dd3e20f1b20e899ce8e3ebf483
Roman Tomjak <35921129+heyroman@users.noreply.github.com>
2019-03-29 15:51:29 +0000

    another commit

--- shopping_cart.py
+++ shopping_cart.py
@@ -20,24 +20,24 @@ class ShoppingCart:
         total_price = 0
         c = Counter(self.products)
 
-        total_price += self._bulk_discounts(c)
-        total_price += self._buy_one_get_one_free(c)
+        total_price += self._bulk_discounts(c, 'Chair')
+        total_price += self._buy_one_get_one_free(c, 'Cushion')
         total_price += c['Sofa'] * 500
 
         return total_price
     
-    def _bulk_discounts(self, c):
+    def _bulk_discounts(self, c, item):
         bulk_discount = 0
 
-        if c['Chair'] >= 3:
-            bulk_discount = c['Chair'] * 90
-        elif c['Chair']:
-            bulk_discount = c['Chair'] * 100
+        if c[item] >= 3:
+            bulk_discount = c[item] * 90
+        elif c[item]:
+            bulk_discount = c[item] * 100
         
         return bulk_discount
 
-    def _buy_one_get_one_free(self, c):
-        q, r = divmod(c['Cushion'], 2)
+    def _buy_one_get_one_free(self, c, item):
+        q, r = divmod(c[item], 2)
         return (q + r) * 25
 
 
